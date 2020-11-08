dbad3f8a59bf27616e372dd929c3ef3ccb99a5a9
Zach <zbaruch20@gmail.com>
2020-11-08 18:46:48 -0500

    Implemented letter_stack.rb, began letter_stack_test.rb

--- game/letter_stack.rb
+++ game/letter_stack.rb
@@ -3,11 +3,57 @@ require_relative 'frequencies'
 # Implementation of LetterStack class.
 # This class represents a stack of letter tiles to be used in the Countdown game. The user
 # can choose whether the LetterStack contains consonants or vowels when initializing it. The
-# frequencies for consonants and vowels can be found in frequencies.rb.
+# frequencies for consonants and vowels can be found in `frequencies.rb`.
 #
 # @author Zach Baruch
 module Letters
-    class LetterStack
+    class LetterStack < Enumerable
+
+        # Constructor.
+        #
+        # @param type [Symbol] the type of LetterStack to be created, `:vowels` or `:consonants`.
+        #   By default, this will be `:vowels`. If this is not one of the valid values, an
+        #   `ArgumentError` will be raised.
+        def initialize(type = :vowels)
+            raise ArgumentError.new('Violation of: type is invalid') unless (type == :vowels or type == :consonants)
+
+            @rep = [] # self is represnted as an Array<String>
+
+            # The frequencies for this LetterStack are based on type
+            # Doing it this way prevents redundant code
+            frequencies = (@@Vowel_frequencies if type == :vowels) || @@Consonant_frequencies
+
+            # For each letter (key), add it to @rep value times (the frequency)
+            frequencies.each_key do |x|
+                frequencies[x].times {|i| @rep << x}
+            end
+
+            @rep.shuffle! # Mix up the tiles
+        end
+
+        # Implementation of `Enumerable#each`
+        def each()
+            @rep.each
+        end
+
+        # Reports the length of `self`.
+        #
+        # @return [Integer] the number of letters in `self`
+        def length()
+            @rep.length
+        end
+
+        # Removes the first letter from `self` and returns it.
+        #
+        # @return [String] the first letter from `self`
+        def draw()
+            @rep.pop
+        end
+
+        # Shuffle elements in `self` in place.
+        def shuffle!()
+            @rep.shuffle!
+        end
 
     end
 end
