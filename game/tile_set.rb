235d785ac7f5df8e714a5732123284ce72901d2b
Zach <zbaruch20@gmail.com>
2020-11-09 02:33:04 -0500

    Created tile_set.rb
    Nothing is implemented yet.

---
+++ game/tile_set.rb
@@ -0,0 +1,10 @@
+# Implementation of TileSet class.
+# This class represents a set of letter tiles to be used as the "letters in play" in the Countdown
+# game. All of the drawn letters should be added to a TileSet, and it can be used to check if
+# the player's guess contains only the letters in play.
+module Letters
+    class TileSet
+        include Enumerable
+
+    end
+end
